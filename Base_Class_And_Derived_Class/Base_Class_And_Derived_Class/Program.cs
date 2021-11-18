093e070a687f6c60b3e658e1616ce8eb35c3b3c3
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-19 00:58:47 +0530

    Add files via upload

---
+++ Base_Class_And_Derived_Class/Base_Class_And_Derived_Class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Base_Class_And_Derived_Class
+{
+    class A
+    {
+        public int a;
+        public A()
+        {
+            a = 5;
+        }
+    }
+    class B : A
+    {
+        public int b;
+        public B()
+        {
+            b = 20; 
+        }
+        public int getsum()
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B Obj = new B();
+            Console.WriteLine("Sum = {0} ",Obj.getsum());
+            Console.ReadKey();
+        }
+    }
+}
