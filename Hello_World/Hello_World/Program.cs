970e1e653f0be6bc87c3b21c01fcd5f6aabb181e
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-28 00:49:37 +0530

    Add files via upload

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Hello World !!!");
+            Console.ReadKey();
+        }
+    }
+}
