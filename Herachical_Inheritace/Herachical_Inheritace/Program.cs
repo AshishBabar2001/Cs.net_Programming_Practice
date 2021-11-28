638b36d155c732d482bf0d891be0b4df5ba60dbd
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-29 00:58:55 +0530

    Add files via upload

---
+++ Herachical_Inheritace/Herachical_Inheritace/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Herachical_Inheritace
+{
+        class A
+        {
+            public int a;
+            public A()
+            {
+                a = 10;
+            }
+        }
+        class B : A
+        {
+            public int b;
+            public B()
+            {
+                b = 7;
+            }
+        }
+        class C : A
+        {
+            public int c;
+            public C()
+            {
+                c = 23;
+            }
+        }
+
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Herachical Inheritance");
+            B obj = new B();
+            C obj2 = new C();
+            Console.WriteLine("Using Class B Object (obj)");
+            Console.WriteLine("a = {0}",obj.a);
+            Console.WriteLine("b = {0}",obj.b);
+            Console.WriteLine("Using Class C Object (obj2)");
+            Console.WriteLine("a = {0}",obj2.a);
+            Console.WriteLine("c = {0}",obj2.c);
+
+            Console.ReadKey();
+        }
+    }
+}
