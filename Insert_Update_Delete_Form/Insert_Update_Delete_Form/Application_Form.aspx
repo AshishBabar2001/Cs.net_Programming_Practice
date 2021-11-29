6b01266f342fb490c762063c139043fd0fd6451d
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-11-30 00:40:33 +0530

    Add files via upload

---
+++ Insert_Update_Delete_Form/Insert_Update_Delete_Form/Application_Form.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Application_Form.aspx.cs" Inherits="Insert_Update_Delete_Form.Application_Form" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title> Student Registration</title>
+    <style type="text/css">
+        .style1
+        {
+            width: 130px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <center>
+    <tital>Student Registration</title>
+    <table>
+    <tr>
+    
+    <td class="td">ID:</td>  
+            <td>  
+                <asp:TextBox ID="txtID" runat="server" Width="200px"></asp:TextBox></td>
+                
+                
+            <td class="style1">
+    </tr>
+    <tr>  
+            <td class="td">Name:</td>  
+            <td>  
+                <asp:TextBox ID="txtName" runat="server" Width="201px"></asp:TextBox></td>  
+            <td class="style1">   
+        </tr>
+        <tr>  
+            <td class="td">Address:</td>  
+            <td>  
+                <asp:TextBox ID="txtAddress" runat="server" Width="200px"></asp:TextBox></td>  
+            <td class="style1"> </td>  
+        </tr>
+        <tr>  
+            <td class="td">Mobile:</td>  
+            <td>  
+                <asp:TextBox ID="txtMobile" runat="server" Width="201px"></asp:TextBox></td>  
+            <td class="style1"> </td>  
+        </tr>
+        <tr>  
+            <td class="td">Email ID:</td>  
+            <td>  
+                <asp:TextBox ID="txtEmail" runat="server" Width="199px"></asp:TextBox></td>  
+            <td class="style1"> </td>  
+        </tr>
+        <tr>  
+            <td></td>  
+            <td>  
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" /> 
+                <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" /> 
+              </td> 
+            <td class="style1"></td>  
+        </tr>
+        </table>
+        <div>
+        </center>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Insert_Update_Delete_Form/Insert_Update_Delete_Form/Application_Form.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Insert_Update_Delete_Form
+{
+    public partial class Application_Form : System.Web.UI.Page
+    {
+        SqlConnection Con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=Student_Register_DB;Integrated Security=True");
+        
+        void SCon_Open()
+        {
+            if (true)
+            {
+                Con.Open();
+            }       
+        }
+
+        void SCon_Closed()
+        {
+           Con.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtID.Text = Convert.ToString(Auto_Incr());
+        }
+
+        int Auto_Incr()
+        {
+            SCon_Open();
+
+            SqlCommand Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+            Cmd.CommandText = "Select Count(*) From Students_Details ";
+
+            int Cnt = Convert.ToInt32(Cmd.ExecuteScalar());
+
+            Cmd.Dispose();
+
+            if (Cnt > 0)
+            {
+                Cmd.Connection = Con;
+                Cmd.CommandText = "Select Max(SID) From Students_Details ";
+
+                Cnt = Convert.ToInt32(Cmd.ExecuteScalar());
+
+                Cnt = Cnt + 1;
+            }
+            else
+            {
+                Cnt = 1;
+
+            }
+
+            SCon_Closed();
+
+            return Cnt;
+        }
+
+        void ShowMessage(string msg)  
+        {  
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('" + msg + "');</script>");  
+        }  
+        void clear()  
+        {  
+            txtID.Text = Convert.ToString(Auto_Incr()); 
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtMobile.Text = "";  
+            txtEmail.Text = "";  
+            txtName.Focus();  
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        { 
+                SCon_Open();
+                if (txtName.Text != "" && txtMobile.Text != "" && txtEmail.Text != "" && txtAddress.Text != "")
+                {
+                    SqlCommand cmd = new SqlCommand("Insert into Students_Details (SID,Name,Address,Mobile,Email ) values (@ID,@Name,@Address,@Mobile,@Email)", Con);
+                    cmd.Parameters.AddWithValue("@ID", txtID.Text);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Dispose();
+                    ShowMessage("Registered successfully......!");
+
+                    clear(); 
+                }
+                else
+                {
+                    ShowMessage("Please, Fill All The Records");
+                }
+
+                SCon_Closed();
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+              
+                SCon_Open();  
+
+                if (txtID.Text != "")
+            	{
+		        SqlCommand cmd = new SqlCommand("update Students_Details Set Name=@Name,Address=@Address,Mobile=@Mobile,Email=@Email where SID = @SID", Con);  
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);  
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);  
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);  
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);  
+                cmd.Parameters.AddWithValue("@SID",txtID.Text);  
+                cmd.ExecuteNonQuery();  
+                cmd.Dispose(); 
+ 
+                ShowMessage("Student Data update Successfully......!"); 
+	            } 
+                else
+	            {
+                    ShowMessage("Enter Roll No First");
+	            }
+
+                SCon_Closed();
+            }
+    }
+}
