d3034534b3efa869962850460611b5c2ecdf83b7
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-02 00:29:51 +0530

    Add files via upload

---
+++ Palindrome_Or_Not/Palindrome_Or_Not/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Or_Not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, Rev = 0, R, N1;
+            Console.WriteLine("Enter Any Number");
+            N = Convert.ToInt32(Console.ReadLine());
+            N1 = N;
+            while (N > 0)
+            {
+                R = N % 10;
+                Rev = Rev * 10 + R;
+                N = N / 10;
+            }
+            if (N1 == Rev)
+            {
+                Console.WriteLine("Number Is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number Is Not Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
