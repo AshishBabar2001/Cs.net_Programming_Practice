f0f8e85d36cc55cb66a11ce76157d9a96407dcfa
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2021-12-03 01:35:58 +0530

    Add files via upload

---
+++ Pass_By_Value/Pass_By_Value/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pass_By_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10, B = 20;
+            int C;
+            Console.WriteLine("Before Swapping Of Value A Is : "+A);
+            Console.WriteLine("Before Swapping Of Value B Is : "+B);
+            C = A;
+            A = B;
+            B = C;
+            Console.WriteLine("\nAfter Swapping Value Of A Is : "+A);
+            Console.WriteLine("After Swapping Value Of B Is : "+B);
+            Console.ReadKey();
+        }
+    }
+}
