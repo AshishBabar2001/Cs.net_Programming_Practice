050bf039bf84245e5b192092fcbb03a0e5910078
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-01-12 02:19:18 +0530

    Add files via upload

---
+++ Swapping_By_Referance/Swapping_By_Referance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_By_Referance
+{
+    class Program
+    {
+        static void Swap(ref int X, ref int Y)
+        {
+            int t = X;
+            X = Y;
+            Y = t;
+        }
+        static void Main(string[] args)
+        {
+            int a = 50, b = 40;
+            Console.WriteLine("Value Of a And b Before Swapping");
+            Console.WriteLine("a = "+a);
+            Console.WriteLine("b = "+b);
+
+            Swap(ref a,ref b);
+            Console.WriteLine("Value Of a And b After Swapping");
+            Console.WriteLine("a = "+a);
+            Console.WriteLine("b = "+b);
+            Console.ReadKey();
+        }
+    }
+}
